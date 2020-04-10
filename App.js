18b15c75a593187b03595b450e0c5e9a24b44855
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2020-04-10 16:05:18 +0200

    First commit - basic navigation done

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import HomeScreen from './screens/home.js';
+import TrainScreen from './screens/train.js';
+import CreateScreen from './screens/create.js';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Train' component={TrainScreen} />
+        <Stack.Screen name='Create' component={CreateScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Button 
+        msg='Train' 
+        color='#F44'
+        onPress={() => navigation.navigate('Train')} />
+
+      <Button 
+        msg='Create a Workout'
+        color='#4F8'
+        onPress={() => navigation.navigate('Create')} />
+    </View>
+  );
+}
+
+function Button({ msg, color, onPress }) {
+  return (
+    <TouchableOpacity 
+      style={[styles.buttons, { backgroundColor: color}]}
+      onPress={onPress}>
+
+      <Text style={styles.buttonText}> {msg} </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttons: {
+    width: 200,
+    height: 50,
+    margin: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonText: {
+    fontSize: 18,
+  },
+});
